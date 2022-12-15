6c4434d011fea05f098978b5535df43594a121d0
robinbaud <72597879+robinbaud@users.noreply.github.com>
2022-12-15 05:05:49 +0100

    ajoue plein de truc

--- rpgaming/src/App.js
+++ rpgaming/src/App.js
@@ -1,44 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Banner, StaticBanner } from "material-ui-banner";
-import { slide as Menu } from "react-burger-menu";
-import TableCell from "@mui/material/TableCell";
-import { Typography } from "@mui/material";
+
+import { Button, Typography } from "@mui/material";
+import EventScrollbar from "./components/EventScollbar";
+import StandScrollbar from "./components/StandScollbar";
+
 function App() {
   return (
     <div className="App">
       <div className="Menu"></div>
-      <div className="Banner">
-        <Typography>RPGaming Fest!</Typography>
-        <Typography
-          align="left"
-          variant="h2"
-          style={{ fontFamily: "Enchanted-Land" }}
-        >
-          Stands à découvrir
-        </Typography>
-        <div className="standScrollable">
-          <TableCell className="stand img1" style={{ padding: 0 }}>
-            <div className="standInfo">
-              <Typography variant="body1">Titre</Typography>
-            </div>
-          </TableCell>
-          <TableCell className="stand img2" style={{ padding: 0 }}>
-            <div className="standInfo"></div>
-          </TableCell>
-          <TableCell className="stand img3" style={{ padding: 0 }}>
-            <div className="standInfo"></div>
-          </TableCell>
-          <TableCell className="stand img4" style={{ padding: 0 }}>
-            <div className="standInfo"></div>
-          </TableCell>
-        </div>
-        <scrollbars sc>
-          <div>
-            <img src="" alt="" />
-          </div>
-        </scrollbars>
+      <Typography>RPGaming Fest!</Typography>
+      <div style={{ display: "flex", flexDirection: "row", width: "50vw" }}>
+        <EventScrollbar />
+        <StandScrollbar />
       </div>
+      <Button
+        variant="outlined"
+        color="error"
+        href="https://www.eventbrite.fr/"
+        target="_blank"
+      >
+        Réserver vos billet
+      </Button>
     </div>
   );
 }
---
+++ rpgaming/src/components/EventScollbar.js
@@ -0,0 +1,58 @@
+import "../App.css";
+import TableCell from "@mui/material/TableCell";
+import { Typography } from "@mui/material";
+function EventScrollbar() {
+  return (
+    <div className="container">
+      <Typography
+        align="left"
+        variant="h4"
+        style={{ fontFamily: "Poppins", fontWeight: "bold" }}
+      >
+        Stands à découvrir
+      </Typography>
+
+      <div className="standScrollable" style={{ overflowY: "scroll" }}>
+        <TableCell className="stand img1" style={{ padding: 0 }}>
+          <div className="standInfo">
+            <Typography variant="h3" style={{ fontFamily: "Poppins" }}>
+              Titre
+            </Typography>
+            <Typography variant="body1" style={{ fontFamily: "Poppins" }}>
+              9h - 11h
+            </Typography>
+            <Typography variant="body1" style={{ fontFamily: "Poppins" }}>
+              stand 55
+            </Typography>
+          </div>
+        </TableCell>
+        <TableCell className="stand img2" style={{ padding: 0 }}>
+          <div className="standInfo"></div>
+        </TableCell>
+        <TableCell className="stand img3" style={{ padding: 0 }}>
+          <div className="standInfo"></div>
+        </TableCell>
+        <TableCell className="stand img4" style={{ padding: 0 }}>
+          <div className="standInfo"></div>
+        </TableCell>
+        <TableCell className="stand img4" style={{ padding: 0 }}>
+          <div className="standInfo"></div>
+        </TableCell>
+        <TableCell className="stand img4" style={{ padding: 0 }}>
+          <div className="standInfo"></div>
+        </TableCell>
+        <TableCell className="stand img4" style={{ padding: 0 }}>
+          <div className="standInfo"></div>
+        </TableCell>
+        <TableCell className="stand img4" style={{ padding: 0 }}>
+          <div className="standInfo"></div>
+        </TableCell>
+        <TableCell className="stand img4" style={{ padding: 0 }}>
+          <div className="standInfo"></div>
+        </TableCell>
+      </div>
+    </div>
+  );
+}
+
+export default EventScrollbar;
